68552655983ca725e87967ee6b6996059571f05d
Claire Castanet <clairecastanet@MASTAFF-Claire-Castanet-Q14CH4R1PW.local>
2025-06-15 15:39:43 +0100

    Figuring out the layer system in the Jumper display

--- src/components/pattern-data-entry/data-entry-children/JumperSelectionCanvas.jsx
+++ src/components/pattern-data-entry/data-entry-children/JumperSelectionCanvas.jsx
@@ -1,8 +1,10 @@
 import raglanJumperBody from "./jumper-selection-img-layers/raglan_jumper_body.png";
 import raglanJumperRoundNeck from "./jumper-selection-img-layers/raglan_round_neckline (1).png";
+import raglanJumperVNeck from "./jumper-selection-img-layers/raglan_v_neck (1).png";
 import { Stage, Layer, Image } from "react-konva";
 import useImage from "use-image";
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useContext } from "react";
+import { FinalJumperDataContext } from "../../../contexts/FinalJumperDataContext";
 
 const URLImage = ({ src, ...rest }) => {
   const [image] = useImage(src, "anonymous");
@@ -12,7 +14,11 @@ const URLImage = ({ src, ...rest }) => {
 const JumperSelectionCanvas = () => {
   const containerRef = useRef(null);
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
-  const bool = true;
+  const { finalJumperData, setFinalJumperData } = useContext(
+    FinalJumperDataContext
+  );
+  const [imageNode, setImageNode] = useState([]);
+
 
   useEffect(() => {
     const resize = () => {
@@ -26,22 +32,72 @@ const JumperSelectionCanvas = () => {
 
     resize(); // Initial size
     window.addEventListener('resize', resize); // Resize on window change
-    return () => window.removeEventListener('resize', resize);
+    return () => {
+      window.removeEventListener('resize', resize)
+    }
   }, []);
 
-  console.log(dimensions);
+  useEffect(() => {
+    const jumperLayers = [];
+
+    if (!finalJumperData.jumperShape &&
+      !finalJumperData.necklineShape) { 
+      jumperLayers.push(
+        <URLImage
+          key="body"
+          src={raglanJumperBody}
+          x={0}
+          width={dimensions.width}
+          height={dimensions.height}
+        />)
+      jumperLayers.push(
+        <URLImage
+          key="neck" src={raglanJumperRoundNeck}
+          x={0}
+          width={dimensions.width}
+          height={dimensions.height} />
+      );
+    }
+    
+    if (finalJumperData.jumperShape === "top-down-raglan") {
+      jumperLayers.push(<URLImage
+      key="body"
+      src={raglanJumperBody}
+      x={0}
+      width={dimensions.width}
+      height={dimensions.height}
+    />)
+    }
+    
+    if (finalJumperData.necklineShape === "round-neck") {
+      jumperLayers.push(<URLImage
+      key="neck" src={raglanJumperRoundNeck}
+      x={0}
+      width={dimensions.width}
+      height={dimensions.height} />)
+    }
+
+    if (finalJumperData.necklineShape === "v-shape") { 
+      jumperLayers.push(
+        <URLImage
+        key="neck" src={raglanJumperVNeck}
+        x={0}
+        width={dimensions.width}
+          height={dimensions.height} />
+      )
+    }
+   
+    setImageNode(jumperLayers);
+  
+
+  }, [finalJumperData, dimensions]);
+
 
   return (
     <div ref={containerRef} id="jumper-selection-canvas" >
       <Stage width={dimensions.width} height={dimensions.height}>
         <Layer>
-        {bool
-          ? [
-              <URLImage key="body" src={raglanJumperBody} x={0} width={dimensions.width} height={dimensions.height} />,
-              <URLImage key="neck" src={raglanJumperRoundNeck} x={0} width={dimensions.width} height={dimensions.height} />
-            ]
-          : <URLImage src={raglanJumperBody} x={150} />
-        }
+          {imageNode}
         </Layer>
       </Stage>
       </div>
@@ -49,3 +105,8 @@ const JumperSelectionCanvas = () => {
 }
 
 export default JumperSelectionCanvas;
+
+// [
+//   <URLImage key="body" src={raglanJumperBody} x={0} width={dimensions.width} height={dimensions.height} />,
+//   <URLImage key="neck" src={raglanJumperRoundNeck} x={0} width={dimensions.width} height={dimensions.height} />
+// ]
