d8aa2647872654297563c3e9e4a6a5c793d36522
Claire Castanet <claire.castanet@outlook.com>
2025-08-20 20:15:56 +0100

    added generic name to pattern when user is not logged-in

--- src/components/pattern-templates/PatternTemplate.jsx
+++ src/components/pattern-templates/PatternTemplate.jsx
@@ -1,13 +1,18 @@
 import PatternSection from "./PatternSection";
 
 //Each pattern section is being rendered from the sectionTitle, the markdown above and the pattern data. 
+const genericPatternTitle = {
+  "top-down-raglan": "Top-Down Raglan Pattern",
+  "bottom-up": "Bottom-up Raglan Pattern",
+  "drop-shoulder": "Drop-shoulder Seamed Jumper"
+}
+
 const PatternTemplate = ({ patternData, patternSections }) => {
-  console.log(patternData)
   return (
     <div className="pageBackground">
       <div className="pageShaper">
         <div className="pattern-template-container">
-          <h1 style={{ textAlign: "center", marginBottom: "10%" }}>{patternData.patternName}</h1>
+          <h1 style={{ textAlign: "center", marginBottom: "10%" }}>{patternData?.patternName || genericPatternTitle[patternData.jumperShape]}</h1>
           {patternSections.map((patternSection) => {
             return <PatternSection id={patternSection.sectionTitle} patternSection={patternSection} patternData={patternData} />
           })}
