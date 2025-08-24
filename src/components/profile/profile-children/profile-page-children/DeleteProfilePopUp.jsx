c8a6287db48649800f1d2ed97af000b3eb0da4fe
Claire Castanet <claire.castanet@outlook.com>
2025-08-24 20:58:18 +0100

    deletion functionality is coded in

--- src/components/profile/profile-children/profile-page-children/DeleteProfilePopUp.jsx
+++ src/components/profile/profile-children/profile-page-children/DeleteProfilePopUp.jsx
@@ -1,28 +1,72 @@
+import { deleteUserData } from "../../../../services-and-util-functions/user-services";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import BinSvgIcon from "../../profile-assets/trash-svgrepo-com.svg?react";
 
 const DeleteProfilePopUp = ({ togglePopUp, setTogglePopUp }) => {
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [successMsg, setSuccessMsg] = useState(null);
+    const navigate = useNavigate();
+
     const handleClickCancel = () => {
         setTogglePopUp(false);
     }
 
-    const handleClickDelete = () => {
-        //TODO: write the functionality
+    const handleClickDelete = async () => {
+        try {
+            await deleteUserData();
+            setSuccessMsg("User successfully deleted!")
+        } catch (error) {
+            setErrorMsg("Account deletion failed, try again later");
+        }
     }
 
-    if (!togglePopUp) return null; 
+    const handleClickBackHomepage = () => {
+        navigate("/")
+    }
+
+    if (!togglePopUp) return null;
+
+    if (errorMsg) {
+        return (
+            <div className="pop-up-overlay">
+                <div className="pop-up">
+                    <BinSvgIcon id="delete-profile-pop-up-icon" />
+                    <h3>{errorMsg}</h3>
+                    <div>
+                        <button className="main-button-style" style={{ marginRight: "1em" }} onClick={handleClickCancel} >Close</button>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
+    if (successMsg) {
+        return (
+            <div className="pop-up-overlay">
+                <div className="pop-up">
+                    <BinSvgIcon id="delete-profile-pop-up-icon" />
+                    <h3>{successMsg}</h3>
+                    <div>
+                        <button className="main-button-style" style={{ marginRight: "1em" }} onClick={handleClickBackHomepage}>Go back to homepage</button>
+                    </div>
+                </div>
+            </div>
+        );
+    };
 
     return (
         <div className="pop-up-overlay">
-             <div className="pop-up">
-            <BinSvgIcon id="delete-profile-pop-up-icon" />
-            <h3>Are you sure you want to delete your account?</h3>
-            <div>
-                <button className="main-button-style" style={{marginRight: "1em" }} onClick={handleClickCancel} >No, cancel</button>
-                <button className="main-button-style button-style-red" onClick={handleClickDelete}>Yes, delete</button>
+            <div className="pop-up">
+                <BinSvgIcon id="delete-profile-pop-up-icon" />
+                <h3>Are you sure you want to delete your account?</h3>
+                <div>
+                    <button className="main-button-style" style={{ marginRight: "1em" }} onClick={handleClickCancel} >No, cancel</button>
+                    <button className="main-button-style button-style-red" onClick={handleClickDelete}>Yes, delete</button>
+                </div>
             </div>
         </div>
-        </div>  
-    )
-}
+    );
+};
 
 export default DeleteProfilePopUp;
