01c2e7f63044eeb2730b3d58a081f8e9fa113d96
Claire Castanet <claire.castanet@outlook.com>
2026-04-30 12:19:47 +0100

    commit before merging latest from main

---
+++ src/features/profile/profile-children/EditEmailAddress.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const EditEmailAddress = () => {
+    const [formData, setFormData] = useState({});
+
+    console.log(formData);
+
+    const handleSubmitEmailChangeRequest = (event) => {
+        event.preventDefault();
+        //open a fucking pop-up 
+    };
+
+    const handleInput = (event) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    } 
+
+    return (
+        <div className="edit-account-tile standard-box-styling">
+            <h3>Edit your e-mail address:</h3>
+            <p>We will send a confirmation e-mail to your new address. Your current address will not be updated until you click on the confirmation link attached to the e-mail.</p>
+            <form className="edit-account-form-section" onSubmit={handleSubmitEmailChangeRequest}>
+                <div className="edit-account-inputs-section">
+                    <div className="edit-account-input">
+                        <label htmlFor="new-email"><b>Enter new e-mail:</b></label>
+                        <input id="new-email" name="newEmail" type="email" onChange={handleInput} />
+                    </div>
+                
+                    <div className="edit-account-input">
+                        <label htmlFor="confirm-email"><b>Confirm new e-mail:</b></label>
+                        <input id="confirm-email" name="confirmedNewEmail" type="email" onChange={handleInput}/>
+                    </div>
+
+                    <div className="edit-account-input">
+                        <label htmlFor="confirm-email"><b>Enter password:</b></label>
+                        <input id="enter-password" name="password" type="password" onChange={handleInput}/>
+                    </div>
+                </div>
+
+                <button type="submit" className="main-button-style button-style-purple edit-profile-button">
+                    Send confirmation e-mail to new address
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default EditEmailAddress;
